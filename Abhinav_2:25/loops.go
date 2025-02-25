9c1e3d2c885e1d8221536e7989bac2d283d5987e
Abhinav Randive <arandive27@wooster.edu>
2025-02-25 15:44:25 -0500

    Loops Update

--- Abhinav_2:25/loops.go
+++ Abhinav_2:25/loops.go
@@ -4,76 +4,76 @@ import "fmt"
 
 //func main() {
 
-    // for loop
+// for loop
 
-    /* for i := 0; i < 5; i++ {
-        if i == 3 {
-            // continue
-            break
-        }
-        fmt.Println(i)
-    } */
+/* for i := 0; i < 5; i++ {
+    if i == 3 {
+        // continue
+        break
+    }
+    fmt.Println(i)
+} */
 
-    //nested loops
-    /* adj := [2]string{ "big", "tasty"}
-    fruits := [3]string{"apple", "banana", "orange"}
-    for i := 0; i < len(adj); i++ {
-        for j := 0; j < len(fruits); j++ {
-            fmt.Println(adj[i], fruits[j])
-        }
-    } */
-    
-   /*  fruits := [3]string{"apple", "banana", "orange"}
+//nested loops
+/* adj := [2]string{ "big", "tasty"}
+   fruits := [3]string{"apple", "banana", "orange"}
+   for i := 0; i < len(adj); i++ {
+       for j := 0; j < len(fruits); j++ {
+           fmt.Println(adj[i], fruits[j])
+       }
+   } */
+
+/*  fruits := [3]string{"apple", "banana", "orange"}
     for _, val := range fruits {
         fmt.Printf( "%v\n", val)
     } */
-    
-    //--------------------------------------------------------------------------------
 
-    /* number := 10
+//--------------------------------------------------------------------------------
 
-    if number > 0 {
-        fmt.Println("Number is positive")
-    } else if number < 0 {
-        fmt.Println("Number is negative")
-    } else {
-        fmt.Println("Number is zero")
-    }
+/* number := 10
 
-    //--------------------------------------------------------------------------------
-    if number%2 == 0 {
-        fmt.Println("Number is even")
-    } else {
-        fmt.Println("Number is odd")
-    } 
+   if number > 0 {
+       fmt.Println("Number is positive")
+   } else if number < 0 {
+       fmt.Println("Number is negative")
+   } else {
+       fmt.Println("Number is zero")
+   }
 
-    //--------------------------------------------------------------------------------
-    num1 := 5
-    num2 := 10
+   //--------------------------------------------------------------------------------
+   if number%2 == 0 {
+       fmt.Println("Number is even")
+   } else {
+       fmt.Println("Number is odd")
+   }
 
-    if num1 > num2 {
-        fmt.Println("Num1 is greater than Num2")
-    } else if num2 > num1 {
-        fmt.Println("Num2 is greater than Num1")
-    } else {
-        fmt.Println("Both numbers are equal")
-    } 
+   //--------------------------------------------------------------------------------
+   num1 := 5
+   num2 := 10
 
-    //--------------------------------------------------------------------------------
-    fruits := [3]string{"apple", "banana", "orange"}
-    for _, val := range fruits {
-        fmt.Printf( "%v\n", val)
-    }
+   if num1 > num2 {
+       fmt.Println("Num1 is greater than Num2")
+   } else if num2 > num1 {
+       fmt.Println("Num2 is greater than Num1")
+   } else {
+       fmt.Println("Both numbers are equal")
+   }
 
-    //--------------------------------------------------------------------------------
-    a := 20
-    b := 4
-    fmt.Printf("Addition: %d + %d = %d\n", a, b, a+b)
-    fmt.Printf("Subtraction: %d - %d = %d\n", a, b, a-b)
-    fmt.Printf("Multiplication: %d * %d = %d\n", a, b, a*b)
-    fmt.Printf("Division: %d / %d = %d\n", a, b, a/b) */
+   //--------------------------------------------------------------------------------
+   fruits := [3]string{"apple", "banana", "orange"}
+   for _, val := range fruits {
+       fmt.Printf( "%v\n", val)
+   }
 
-    //--------------------------------------------------------------------------------
+   //--------------------------------------------------------------------------------
+   a := 20
+   b := 4
+   fmt.Printf("Addition: %d + %d = %d\n", a, b, a+b)
+   fmt.Printf("Subtraction: %d - %d = %d\n", a, b, a-b)
+   fmt.Printf("Multiplication: %d * %d = %d\n", a, b, a*b)
+   fmt.Printf("Division: %d / %d = %d\n", a, b, a/b) */
+
+//--------------------------------------------------------------------------------
 //}
 
 /* func myMessage() {
@@ -93,10 +93,32 @@ func main() {
         myMessage()
 } */
 
-func  myFunction(x int, y int) int {
-    return x + y
+/* func  myFunction(x int, y int) (result int){
+    result = x + y
+    return
 }
 
 func main() {
     fmt.Println(myFunction(5, 6))
+} */
+/* func myFunction(x int, y string) (result int, txt1 string) {
+  result = x + x
+  txt1 = y + " World!"
+  return
+}
+
+func main() {
+  fmt.Println(myFunction(5, "Hello"))
+} */
+
+func testcount(x int) int {
+	if x == 12 {
+		return 0
+	}
+	fmt.Println(x)
+	return testcount(x + 1)
+}
+
+func main() {
+	testcount(1)
 }
